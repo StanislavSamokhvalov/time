2e29fa19558317ebae625aedaa4b45210851593f
StanislavSamokhvalov <stanislavsamokhvalov7@gmail.com>
2021-06-20 19:28:08 +0400

    update utf-8

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,33 @@
-val seconds = readLine()!!.toInt()
-val minute = seconds / 60
-val hour = seconds / (60 * 60)
+import kotlin.math.roundToInt
 
-fun endingMinutes(
-    minute: Int
-): String {
-    return when (minute) {
-        1, 21 -> "минуту"
-        2 -> "минуты"
-        else -> "минут"
-    }
+fun secondsToMinutes(
+    seconds: Int
+): Int {
+    return (seconds / 60).toDouble().roundToInt()
 }
 
-fun endingHours(
-    hour: Int
+fun secondsToHours(
+    seconds: Int
+): Int {
+    return (seconds / (60 * 60)).toDouble().roundToInt()
+}
+
+
+fun endingMinutes(
+    seconds: Int
 ): String {
-    return when (hour) {
+    return if (secondsToMinutes(seconds) % 10 == 1) "минуту"
+    else if (secondsToMinutes(seconds) % 10 == 2 || secondsToMinutes(seconds) % 10 == 3 || secondsToMinutes(seconds) % 10 == 4) "минуты"
+    else "минут"
+}
+
+
+fun endingHours(
+    seconds: Int
+): String {
+    return when (secondsToHours(seconds)) {
         1 -> "час"
-        2, 3, 4 -> "часа"
+        2, 3, 4, 22, 23, 24 -> "часа"
         else -> "часов"
     }
 }
@@ -27,8 +37,8 @@ fun agoToText(
 ): String {
     return when (seconds) {
         in 0..60 -> "только что"
-        in 60..(60 * 60) -> "$minute ${endingMinutes(minute)} назад"
-        in 60 * 60 + 1..(60 * 60 * 24) -> "$hour ${endingHours(hour)} назад"
+        in 60..(60 * 60) -> "${secondsToMinutes(seconds)} ${endingMinutes(seconds)} назад"
+        in 60 * 60 + 1..(60 * 60 * 24) -> "${secondsToHours(seconds)} ${endingHours(seconds)} назад"
         in 24 * 60 * 60 + 1..24 * 60 * 60 * 2 -> "сегодня"
         in 24 * 60 * 60 * 2 + 1..24 * 60 * 60 * 3 -> "вчера"
         else -> "давно"
@@ -36,6 +46,9 @@ fun agoToText(
 }
 
 fun main() {
+    println("введи количество секунд")
+    val seconds = readLine()!!.toInt()
     val result = agoToText(seconds)
     print("был(а) в сети $result")
 }
+
